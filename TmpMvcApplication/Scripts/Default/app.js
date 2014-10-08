bee4ec9fb130c20c869cfd243fe38512931eb2e6
Mark Martynov <Mark_Martynov@epam.com>
2014-10-08 19:06:24 +0400

    initial

---
+++ TmpMvcApplication/Scripts/Default/app.js
@@ -0,0 +1,12 @@
+﻿app = angular.module('defaultApp', [
+    'ngRoute'
+]);
+
+app
+    .controller('ctrl', ctrl)
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/test', {
+            templateUrl: '/Scripts/Default/views/test.tpl.html',
+            controller: testCtrl
+        });
+    });
---
+++ TmpMvcApplication/Scripts/Default/controllers/controller.js
@@ -0,0 +1,19 @@
+﻿
+function ctrl($scope) {
+    console.log('main ctrl runned');
+
+    $scope.message = 'msg';
+    $scope.init = function (obj) {
+        console.log('trying to get child scope');
+        console.log(obj);
+    }
+}
+
+function testCtrl($scope) {
+    console.log('test ctrl runned');
+    $scope.message = 'child msg';
+    $scope.items = [
+        { a: 11, b: 'b1' },
+        { a: 12, b: 'b2' },
+        { a: 13, b: 'b3' }];
+}
